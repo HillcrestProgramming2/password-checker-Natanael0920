69ff489b854a2ac83e0b87defbef21c8e7982b31
NaTanael <natanaelbecerra046@gmail.com>
2026-01-08 08:40:06 -0700

    r

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -2,16 +2,54 @@ package java.org.hillcrest.chapter6.password;
 
 public class CriteriaChecker {
 
-public static int evaluateCriteria(String password){
-    String lowercase = "abcdefghijklmnopqrstvuwyz";
-    String uppercase = "ABCDEFGHIJKLMNOPQRSTVUWYZ";
-    int number = 12345678;
-    int score = 0;
-    if(password.length()>=8){
-        score++;
+    public static int evaluateCriteria(String password) {
+        int score = 0;
 
+        // Criterion 1: Length ≥ 8
+        if (password.length() >= 8) {
+            score++;
+        }
+
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        String specialChars = "!@#$%^&*()-+=";
+
+        // Loop through characters
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                hasUpper = true;
+            }
+            if (Character.isLowerCase(c)) {
+                hasLower = true;
+            }
+            if (Character.isDigit(c)) {
+                hasDigit = true;
+            }
+            if (specialChars.indexOf(c) != -1) {
+                hasSpecial = true;
+            }
+        }
+
+        if (hasUpper) score++;
+        if (hasLower) score++;
+        if (hasDigit) score++;
+        if (hasSpecial) score++;
+
+        return score;
     }
 
-}
-    //utility class
+    public static String determineStrength(int score) {
+        if (score <= 2) {
+            return "Weak";
+        } else if (score == 3) {
+            return "Moderate";
+        } else {
+            return "Strong";
+        }
+    }
 }
