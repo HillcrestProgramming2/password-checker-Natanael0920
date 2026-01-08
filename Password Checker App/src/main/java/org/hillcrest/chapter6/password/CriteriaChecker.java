38a8a09bdd352e14a139ee444ad5158119d6dfc8
Silva <nsil5450@go.canyonsdistrict.org>
2026-01-08 07:45:32 -0700

    d

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -1,6 +1,17 @@
-package org.hillcrest.chapter6.password;
+package java.org.hillcrest.chapter6.password;
 
 public class CriteriaChecker {
 
+public static int evaluateCriteria(String password){
+    String lowercase = "abcdefghijklmnopqrstvuwyz";
+    String uppercase = "ABCDEFGHIJKLMNOPQRSTVUWYZ";
+    int number = 12345678;
+    int score = 0;
+    if(password.length()>=8){
+        score++;
+
+    }
+
+}
     //utility class
 }
--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
@@ -1,7 +1,11 @@
-package org.hillcrest.chapter6.password;
+package java.org.hillcrest.chapter6.password;
 
 public class PasswordCheckerApp {
+
     //main method goes here
+    public static void main(String[] args){
+
+    }
     //prompt user for password
     //Check strength
     //Provide user feedback based on the strength
